2625d5f06a0b425c348dbfa3383bfdf4d0789892
Anyutaa <anyuta29032004@mail.ru>
2025-07-04 15:07:09 +0300

    Исправления backend и добавила gitignore в проект

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -1,8 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import * as fs from 'fs';
+import * as path from 'path';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  private dataFilePath = path.join(__dirname, '..', 'data.json');
+
+  getData() {
+    try {
+      const fileContent = fs.readFileSync(this.dataFilePath, 'utf-8');
+      console.log('Данные считались');
+      return JSON.parse(fileContent);
+    } catch (error) {
+      console.error('Ошибка при чтении данных:', error);
+      return { success: false, message: 'Ошибка при чтении данных' };
+    }
   }
 }
